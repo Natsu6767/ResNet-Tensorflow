5e3095a338693c4630c254c89bf32b423bb894d9
Mohit Jain <mohitjain1999@yahoo.com>
2018-07-08 17:31:34 +0400

    Implement Fully Connected layer.

--- layers.py
+++ layers.py
@@ -36,3 +36,26 @@ def max_pool(x, name, filter_height = 2, filter_width = 2,
 
 	return tf.nn.max_pool(x, ksize = [1, filter_height, filter_width, 1],
 		strides = [1, stride, stride, 1], padding = padding, name = name)
+
+def fc_layer(x, input_size, output_size, name, relu = True):
+
+	"""Create a fully connected layer."""
+	
+	with tf.variable_scope(name) as scope:
+
+		# Create tf variables for the weights and biases.
+		W = tf.get_variable('weights', shape = [input_size, output_size],
+			initializer = tf.random_normal_initializer(mean = 0.0, stddev = 0.01))
+
+		b = tf.get_variable('biases', shape = [output_size], initializer = tf.constant_initializer(0.0))
+
+		# Matrix multiply weights and inputs and add biases.
+		z = tf.nn.bias_add(tf.matmul(x, W), b)
+
+		if relu:
+			# Apply ReLu non linearity.
+			a = tf.nn.relu(z)
+			return a
+
+		else:
+			return z
