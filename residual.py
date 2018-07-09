14d96e6e4c6f991fff8e91de5392a0731afab6d7
Mohit Jain <mohitjain1999@yahoo.com>
2018-07-09 11:55:14 +0400

    Add comments.

--- residual.py
+++ residual.py
@@ -1,5 +1,3 @@
-
-
 import tensorflow as tf
 
 from layers import conv_layer
@@ -14,19 +12,22 @@ def residual_block(x, out_channels, projection = False, name = 'residual'):
 	conv1 = conv_layer(x, 3, 3, out_channels, stride = 1, name = '{}_conv1'.format(name))
 	conv2 = conv_layer(conv1, 3, 3, out_channels, stride = 1, name = '{}_conv2'.format(name))
 
+	# What type of shortcut connection to use
 	if input_channels != out_channels:
 		if projection:
 			# Option B: Projection Shortcut
 			# This introduces extra parameters.
 			shortcut = conv_layer(x, 1, 1, out_channels, stride = 1, name = '{}_shortcut'.format(name))
 		else:
-			# Option A: Zero-Padding
+			# Option A: Identity mapping with Zero-Padding
 			# This method doesn't introduce any extra parameters.
 			shortcut = tf.pad(x, [[0, 0], [0, 0], [0, 0], [0, out_channels - input_channels]])
 	else:
+		# Identity mapping.
 		shortcut = x
 
 
+	# Element wise addition.
 	out = conv2 + shortcut
 
 	return out
--- resnet.py
+++ resnet.py
@@ -1,10 +1,15 @@
-"""This is a TensorFlow modified implementation of ResNet by He et all.
+"""This is a TensorFlow implementation of ResNet by He et all.
+
+The architecture is based on the ResNet-n architecture
+(where n = 20, 32, 44, 56) described in the paper to
+perform analysis and tests on CIFAR-10 dataset.
 
 Paper: Deep Residual Learning for Image Recognition
 (https://arxiv.org/abs/1512.03385)
 
 Explanation on ResNet can be found in my blog post:
 https://mohitjain.me/2018/06/13/resnet/
+
 @author: Mohit Jain (contact: mohitjain1999(at)yahoo.com)
 
 """
@@ -28,9 +33,10 @@ class resnet(object):
 			print("ResNet DEPTH INVALID!\n")
 			return
 
-		self.NUM_CONV = int(((n - 20) / 6) + 3)
+		self.NUM_CONV = int(((n - 20) / 6) + 3) # For n = 20, each block will have 3 residual blocks.
 		self.X = x
 		self.NUM_CLASSES = num_classes
+		# Store the last layer of the network graph.
 		self.out = None
 
 		self.create()
@@ -40,24 +46,28 @@ class resnet(object):
 		conv1 = conv_layer(self.X, 3, 3, 16, name = 'conv1')
 		self.out = conv1
 
+		""" All residual blocks use zer-padding
+		for shortcut connections """
+
 		for i in range(self.NUM_CONV):
-			conv2 = residual_block(self.out, 16, name = 'conv2_1_{}'.format(i + 1))
-			self.out = conv2
+			resBlock2 = residual_block(self.out, 16, name = 'resBlock2_{}'.format(i + 1))
+			self.out = resBlock2
 
 		pool2 = max_pool(self.out, name = 'pool2')
 		self.out = pool2
 
 		for i in range(self.NUM_CONV):
-			conv3 = residual_block(self.out, 32, name = 'conv3_1_{}'.format(i + 1))
-			self.out = conv3
+			resBlock3 = residual_block(self.out, 32, name = 'resBlock3_{}'.format(i + 1))
+			self.out = resBlock3
 
 		pool3 = max_pool(self.out, name = 'pool3')
 		self.out = pool3
 
 		for i in range(self.NUM_CONV):
-			conv4 = residual_block(self.out, 64, name = 'conv4_1_{}'.format(i + 1))
-			self.out = conv4
+			resBlock4 = residual_block(self.out, 64, name = 'resBlock4_{}'.format(i + 1))
+			self.out = resBlock4
 
+		# Perform global average pooling to make spatial dimensions as 1x1
 		global_pool = global_average(self.out, name = 'gap')
 		self.out = global_pool
 
